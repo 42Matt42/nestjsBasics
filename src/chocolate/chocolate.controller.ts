6f1101fee54989d16b15db7b1f1179107c5b4681
Matthieu Saubin <matt.saubin@gmail.com>
2020-09-06 19:09:14 +0200

    ADD PATCH, DELETE, and GET with limit & offset

--- src/chocolate/chocolate.controller.ts
+++ src/chocolate/chocolate.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get, Post, Param, Body } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Patch, Delete, Query } from '@nestjs/common';
+// import { response } from 'express';
 
 @Controller('chocolate')
 export class ChocolateController {
   @Get()
-  findAll() {
-    return 'this endpoint findAll()'
+  findAll(@Query() paginationQuery) {
+    const { limit, offset } = paginationQuery;
+
+    return `All chocolate in the Universe ! constraints: Limit: ${limit}, Offset: ${offset}`
   }
+  // @Get()
+  // findAll(@Res() response) {
+  //   response.status(200).send('This endpoint sends a response.status: 200')
+  // }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
@@ -13,7 +20,18 @@ export class ChocolateController {
   }
 
   @Post()
+  // @HttpCode(HttpStatus.GONE)
   create(@Body('name') body: string) {
     return body;
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() body) {
+    return `This endpoint updates the body (${body})for the specified id: #${id}`
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return `This endpoint removes the entry corresponding to id #${id}`
   }
 }
