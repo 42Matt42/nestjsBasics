27def41e337baa796ea0227e9ac126c4f4da8dff
Matthieu Saubin <matt.saubin@gmail.com>
2020-09-06 21:15:02 +0200

    refacto with modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ChocolateController } from './chocolate/chocolate.controller';
-import { ChocolateService } from './chocolate/chocolate.service';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { ChocolateModule } from './modules/chocolate/chocolate.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController, ChocolateController],
-  providers: [AppService, ChocolateService],
+  imports: [ChocolateModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
